e9071f5704f730c92bae1866099e019485dc3b4a
Bernhard R. Link <brlink@debian.org>
2011-02-03 19:35:54 +0100

    call autoreconf in autogen.sh to make it simpler

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 set -e
 rm -f aclocal.m4 libtool config.cache config.status
-echo "Running aclocal..."
-aclocal
-echo "Running autoconf..."
-autoconf
-echo "Running autoheader..."
-autoheader
-echo "Running automake..."
-automake --foreign -a -c
+echo "Running autoreconf -i ..."
+autoreconf -i
 echo "Now you can run ./configure"
 echo "(use --enable-maintainer-mode if you want Makefile.in automatically regenerated)"
